eb7840b964767b90e79da3121266d6e995717a46
You Taein <ytn1574@gmail.com>
2022-10-03 19:00:04 +0900

    6일차 server to server

--- client/src/main/java/com/example/client/service/RestTemplateService.java
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,6 +1,7 @@
 package com.example.client.service;
 
 
+import com.example.client.dto.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,13 +14,18 @@ public class RestTemplateService {
 
     // http://localhost/api/service/hello
     // response
-    public String hello(){
-        URI uri = UriComponentsBuilder.fromUriString("http://localhost:9191").path("/api/server/hello").encode().build().toUri();
+    public UserResponse hello(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9191")
+                .path("/api/server/hello")
+                .queryParam("name", "aaaa")
+                .queryParam("age", 99)
+                .encode().build().toUri();
 
         System.out.println(uri.toString());
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
 
         System.out.println(result.getStatusCode());
         System.out.println(result.getBody());
